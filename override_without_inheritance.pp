8f4e99f276be5cf67df4b9039a65bc15ae860fa1
nfagerlund <nick.fagerlund@gmail.com>
2012-01-03 16:58:10 -0800

    Add a comment clarifying an older manifest.

--- override_without_inheritance.pp
+++ override_without_inheritance.pp
@@ -7,5 +7,8 @@ File['/tmp/overrider'] {
     owner => "nick",
     mode => 0640,
     # content => "override content",
+    # ensure => file,
 }
 
+# The experiment: Can I override attributes without inheriting? 
+# The result: No, but I can add to them. 
