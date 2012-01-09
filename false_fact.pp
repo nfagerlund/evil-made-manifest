ef4ef1dea6a27778b2345bf454ce40943f2eae8e
nfagerlund <nick.fagerlund@gmail.com>
2012-01-09 12:55:58 -0800

    Mention the str2bool function in false_fact.pp

--- false_fact.pp
+++ false_fact.pp
@@ -1,5 +1,6 @@
 # You can never, it turns out, test a boolean fact with a bare "if". That's because they're actually strings, and while false is false, "false" is true. 
-if $is_virtual {
+# Interestingly, stdlib includes a function that can convert strings to booleans! So use that.
+if str2bool($is_virtual) {
   notice("It's virtual!")
 }
 else {
