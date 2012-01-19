002d1d99572ce58bad14cfb0efbd55c3e6c7e46f
nfagerlund <nick.fagerlund@gmail.com>
2012-01-19 14:12:49 -0800

    For some dumb reason there was a node statement in one of my LP manifests. Fix that.

--- learning-manifests/2.file.pp
+++ learning-manifests/2.file.pp
@@ -1,5 +1,3 @@
-# /root/training-manifests/2.file.pp
-node "barn2.magpie.lan" {
 file {'/tmp/test1':
     ensure => present,
     content => "Hi.",
@@ -15,5 +13,4 @@ file {'/tmp/test3':
 
 notify {"I'm notifying you.":}
 notify {"So am I!":}
-}
 
