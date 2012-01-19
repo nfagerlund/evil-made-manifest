b7adba220718c67921194bf8db77a78b67c39501
nfagerlund <nick.fagerlund@gmail.com>
2012-01-19 14:12:28 -0800

    Test defined function w/ native types.

---
+++ defined-builtin.pp
@@ -0,0 +1,4 @@
+# This demonstrates that native types also work w/ defined function. This almost certainly includes custom native types, although I don't have one handy to test with. 
+notice(defined(file))
+notice(defined(fill))
+
