493baac872c1d0c2a4913e2d9474cb4f749bb4de
nfagerlund <nick.fagerlund@gmail.com>
2012-02-08 14:17:04 -0800

    Test how far back the @fact notation works in templates

---
+++ template-facts-with-at.pp
@@ -0,0 +1,3 @@
+# This demonstrates the @fact notation in templates. Wanted to see how far back it was available, and stopped looking at 0.25.0. good enough for me, eh.
+notice(inline_template("<%= @fqdn %>"))
+
