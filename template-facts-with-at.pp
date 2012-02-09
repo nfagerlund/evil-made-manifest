09f5f0174cd698a8fd9d6184c5b5e81f57f57903
nfagerlund <nick.fagerlund@gmail.com>
2012-02-09 12:35:29 -0800

    Whoops, @fact notation doesn't reliably refer to top scope.

--- template-facts-with-at.pp
+++ template-facts-with-at.pp
@@ -1,3 +1,7 @@
 # This demonstrates the @fact notation in templates. Wanted to see how far back it was available, and stopped looking at 0.25.0. good enough for me, eh.
-notice(inline_template("<%= @fqdn %>"))
-
+# UNFORTUNATELY, that instance variable thing doesn't reliably refer to top-scope facts, as see below. 
+class bunkclass {
+  $fqdn = "something bogus"
+  notice(inline_template("<%= @fqdn %>"))
+}
+include bunkclass
