233ceb11ac9e4ed82b949d2eb257b7533636f462
Nick Fagerlund <nick.fagerlund@gmail.com>
2013-07-30 15:20:13 -0700

    Note that include takes an array after 3.0

--- include_array.pp
+++ include_array.pp
@@ -1,5 +1,10 @@
+# Does include accept an array? ONLY IN >=3.0
+# Does include flatten a nested array? ONLY IN >=3.0
 class one { notify {'class one':}}
 class two { notify {'class two':}}
-$myclasses = ['one', 'two']
-include $myclasses
-
+class three { notify {'class three':}}
+class four { notify {'class four':}}
+# $myclasses = ['one', 'two', ['three', 'four']]
+$myclasses = ['one', 'two', 'three', 'four']
+ include $myclasses
+# include one, two, three, four
