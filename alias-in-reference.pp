d00c36354fd34017822f079c274bcfdf27197501
Nick Fagerlund <nick.fagerlund@gmail.com>
2013-07-30 15:21:35 -0700

    Note that aliases don't work with the defined function

--- alias-in-reference.pp
+++ alias-in-reference.pp
@@ -1,18 +1,22 @@
 # Can you use a resource's namevar or alias in a resource reference?
-# AUGH. 
+# AUGH.
 # Yes in a relationship metaparameter
 # No in a chaining statement.
 # Stab me.
+# And of course it doesn't work with defined. See other files starting with "alias-" to see that it doesn't work with anything else either.
 file {'file1':
   path => '/tmp/file1',
   ensure => file,
+  alias => ['othername', 'fourthname'],
 }
 
 file {'file2':
   path => '/tmp/file2',
   ensure => file,
-  before => File['/tmp/file1'],
+  before => File['othername'],
 }
 
-#File['file2'] -> File['/tmp/file1']
+# File['file2'] -> File['othername']
+
+notice( defined(File['othername']) )
 
