c9cc75a30baf50d7aa8c2324399bf0ff35e30e93
Nick Fagerlund <nick.fagerlund@gmail.com>
2013-11-18 13:28:03 -0800

    Yey, trusted hash is now immutable.

--- trusted_hash.pp
+++ trusted_hash.pp
@@ -1,12 +1,12 @@
-# Is the $trusted hash actually immutable? (Tested with a pre-3.4.0 release, where the key's name was clientcert instead of certname.)
+# Is the $trusted hash actually immutable? (Yes, with fixes to PP-773 and PP-765. Tested commit 4622cbeb558020b2a6b7bf390038abf8ed1a3bf1.)
 
 # CALIBRATION PATTERN
-# $myvalue = $trusted['clientcert']
+# $myvalue = $trusted['certname']
 # notify {"From assignment: Certname is ${myvalue}, ok cool.":}
-# notify {"Directly interpolating: Certname is ${trusted['clientcert']}, ok cool.":}
+# notify {"Directly interpolating: Certname is ${trusted['certname']}, ok cool.":}
 
 # Normal hash
-$nottrusted = { 'authenticated' => 'local', 'clientcert' => 'fakepie.lan' }
+$nottrusted = { 'authenticated' => 'local', 'certname' => 'fakepie.lan' }
 # notice(spew($nottrusted))
 
 
@@ -24,11 +24,11 @@ $dumpvar = inline_template("
 # Can you alter an existing element in a normal hash?
 
 ## Directly? NOPE!
-# $nottrusted['clientcert'] = 'Whoops, owned'
+# $nottrusted['certname'] = 'Whoops, owned'
 
 ## With template? YES!
 $dumpvar2 = inline_template("
-<% @nottrusted['clientcert'] = 'Whoops, owned' %> lol
+<% @nottrusted['certname'] = 'Whoops, owned' %> lol
 ")
 
 # notice(spew($nottrusted))
@@ -37,23 +37,23 @@ $dumpvar2 = inline_template("
 
 # Can you add an element to the trusted hash?
 
-## Directly? YES
-$trusted['newkey'] = 'Added new value to trusted!'
+## Directly? NOPE!
+# $trusted['newkey'] = 'Added new value to trusted!'
 
-## With template? YES
-$dumpvar3 = inline_template("
-<% @trusted['other_new_key'] = 'Other new value!' %> lol
-")
+## With template? NOPE!
+# $dumpvar3 = inline_template("
+# <% @trusted['other_new_key'] = 'Other new value!' %> lol
+# ")
 
 # Can you alter an existing element in the trusted hash?
 
 ## Directly? NOPE!
-# $trusted['clientcert'] = 'Whoops, owned'
+# $trusted['certname'] = 'Whoops, owned'
 
-## With template? YES!
-$dumpvar4 = inline_template("
-<% @trusted['clientcert'] = 'Whoops, owned' %> lol
-")
+## With template? NOPE!
+# $dumpvar4 = inline_template("
+# <% @trusted['certname'] = 'Whoops, owned' %> lol
+# ")
 
 
 notice(spew($trusted))
