62fa3ecc80d35c5f1038500f3d1388d827d419f4
Nick Fagerlund <nick.fagerlund@gmail.com>
2014-02-13 16:36:59 -0800

    Add demonstration of validate_cmd working with content and source

---
+++ validate_cmd.pp
@@ -0,0 +1,16 @@
+# You'll have to change the first one to true before testing the second one will work.
+
+# does validate_cmd work with content? YUP
+
+file {'/tmp/content':
+  content => 'something else',
+  validate_cmd => 'false',
+}
+
+# How about with source? YUP
+
+file {'/tmp/source':
+  source => '/tmp/content',
+  validate_cmd => 'false',
+  require => File['/tmp/content'],
+}
