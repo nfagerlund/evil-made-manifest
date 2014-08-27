6554a96157c9a9bf225c99643b4fa67ea3d177d7
Chris Roddy <croddy@puppetlabs.com>
2014-08-27 15:03:32 -0400

    adding more crap
    to make it uglier

--- snack.pp
+++ snack.pp
@@ -1,14 +1,15 @@
-#/etc/puppetlabs/puppet/modules/snicker/snack.pp
- 
+#/tmp/non-module-path/snicker/snack.pp
+      
 class snicker::snack {
   jabberwock { 'gimble':
-    chortle => true,
-    brillig => 'frabjous',
-    birds   => ['jubjub', 'vorpal', "${wabe}"],
+    chortle => true,brillig
+    => 'frabjous',
+    birds   => ['jubjub', "$vorpal", wabe],
   }
-  galumphing { ['mome', 'rath']:
-    borogove => 'bandersnatch',
-    tulgey   => $uffish,
-    mimsy    => 'manxome',
+  galumphing { ['mome', "rath"]:
+    borogove  => bandersnatch,
+     tulgey  => $uffish,
+      mimsy => 'manxome',
   }
 }
+
