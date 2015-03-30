2cce67d85839a465e16b53c122c23c029b9f7a08
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-03-30 10:44:53 -0700

    Class references like Class['mything'] can be given to include

---
+++ future_class_reference.pp
@@ -0,0 +1,10 @@
+# what the hell is a class reference? I'm assuming it's like a resource reference.
+
+class mything {
+
+  notify {"it worked":}
+}
+
+include Class['mything']
+
+# Oh, yup, no prob.
