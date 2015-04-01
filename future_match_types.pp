0afe5fd323b90eb5a8e5f465000395500f1ad6c1
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-04-01 15:47:18 -0700

    Mindblowing future parser Types stugff to BLOW your minb

---
+++ future_match_types.pp
@@ -0,0 +1,11 @@
+# OK, so we know from talking with henrik that File[foo] !~ File, because it's not actually a resource, it's a resource reference. And as such, it's a TYPE, not a value whose type is File. (Same if you used an actual resource declaration behind the ~, because the VALUE of a resource declaration is a resource reference.)
+
+# So,
+
+notice("Is File Type? ${File['foo'] =~ Type}") # yes
+notice("Is File Type[file]? ${File['foo'] =~ Type[File]}") # yes
+notice("Is File Resource? ${File['foo'] =~ Resource}") # no, because it's not a MEMBER, it's a SIBLING.
+notice("Is File Resource File? ${File['foo'] =~ Resource[File]}") # No. Same as above, but even closer siblings.
+notice("Is File Type Resource? ${File['foo'] =~ Type[Resource]}") # Yes.
+notice("Is File foo Type File foo? ${File['foo'] =~ Type[File[foo]]}") # yes, with or without quotes. Looks like it handles the munging.
+notice("Is File foo Type File bar? ${File['foo'] =~ Type[File[bar]]}") # No, as expected.
