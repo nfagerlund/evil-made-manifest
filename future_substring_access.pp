b23f0749df9972d9b8fc620d7d81873de065d42d
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-04-03 15:32:04 -0700

    You can access substrings now.

---
+++ future_substring_access.pp
@@ -0,0 +1,8 @@
+# Whoa, you can use access brackets to get substrings. This is new.
+
+# notice("${'this is my string'[0,6]}")
+$strang = 'this is my string'
+$strung = $strang[0,6]
+notice($strung)
+
+# Documented in the spec in expressions.md, under the heading ### String Value [ ]
