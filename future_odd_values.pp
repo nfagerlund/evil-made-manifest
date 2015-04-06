d3f480e58d366cfddbc4416e4100aa3e03330c63
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-04-06 15:44:43 -0700

    More odd values, inc. chaining statements and convincing it to put a collector in a variable

--- future_odd_values.pp
+++ future_odd_values.pp
@@ -34,3 +34,33 @@ notice(spew( $myamend ))
 # $mycollector = Notify <| |>
 # notice(spew( $mycollector ))
 
+# Value of a chaining statement:
+
+$mychain = (notify {"I come second.":} <- Notify["I'm a notify."])
+notice(spew( $mychain ))
+# Well, in that case it's a single-element array whose only member is the resource reference that came LAST in the statement, regardless of which direction the arrow was pointing. How about some weirder ones?
+
+$myweirdchain = (file {"/tmp/thing": ensure => file,} -> Notify <| |>)
+notice( $myweirdchain )
+# notice(spew( $myweirdchain ))
+# HAHAHAHAHAHAHA HOLY SHITTTTT, IT'S THE ENTIRE FUCKIN UNIVERSE!!! Well, so it
+# assigned the value of the resource collector to a variable. I guess that's the
+# way to get around the rules and do that. It's a
+# Puppet::Pops::Evaluator::Collectors::CatalogCollector:0x007fe5f48d0fb8 And
+# since I guess it has references to the entire catalog state and possibly the
+# rest of the program state, my spew() function dumped it out and it took like 2
+# minutes to run, barfing reams to the terminal. I'm seeing facts in here,
+# modules, environments, bindings, loaders, modules, wow. But it continued without complaint!!!
+
+# Okay, how about a multi-resource reference.
+
+$mymultichain = (notify {"the alpha":} -> Notify["I come second.", "I'm a notify."])
+notice(spew( $mymultichain ))
+# That's an array of resource references equivalent to the multi-reference. That's fine and expected.
+
+# How about a long chain?
+
+$mylongchain = (notify {"long one":} -> Notify["I'm a notify."] -> notify {"long two":})
+notice(spew( $mylongchain ))
+# single element array containing the value of the last operand in the whole chain, in this case Notify["long two"].
+
