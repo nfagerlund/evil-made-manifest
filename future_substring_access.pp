11a96bb0406ffea81b28dd46bc050448bde89c03
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-04-08 17:51:31 -0700

    Show negative indexes with substring access.

--- future_substring_access.pp
+++ future_substring_access.pp
@@ -6,3 +6,11 @@ $strung = $strang[0,6]
 notice($strung)
 
 # Documented in the spec in expressions.md, under the heading ### String Value [ ]
+# It's "string"[<START>,<LENGTH OR OFFSET FROM END>]. Positive second values are lengths, negatives are offsets from end.
+
+
+# The offset from end is 1-indexed, not 0-indexed.
+notice("hi there I'm a string"[1,-2]) # i there I'm a strin
+notice("hi there I'm a string"[-5,2]) # tr
+notice("hi there I'm a string"[-5,-2]) # trin
+notice("hi there I'm a string"[-5,-7]) # (empty string)
