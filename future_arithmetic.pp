62731acb33d8bb8633d77ce6a9199a6e6929105a
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-04-08 17:50:48 -0700

    Some arithmetic testing stuff with integers and floats

---
+++ future_arithmetic.pp
@@ -0,0 +1,14 @@
+notice(1/4) # 0
+notice(1.0/4) # 0.25
+notice(1/4.0) # 0.25
+
+notice(8 * -7.992) # -63.936
+notice((8 * -7.992)/4) # -15.984
+# notice(.25) # still a syntax error.
+notice(0777) # 511
+
+notice(8 * 3e5) # 2400000.0, exponentiation = always a float
+
+# notice(0777.3) # error!
+notice(0x777) # 1911
+notice(0xdef) # 3567
