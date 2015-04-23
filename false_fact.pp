904cc2d1e54e593825b417b12278bd34c38843c3
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-04-23 13:27:01 -0700

    Facts can be multiple data types now

--- false_fact.pp
+++ false_fact.pp
@@ -1,9 +1,12 @@
-# You can never, it turns out, test a boolean fact with a bare "if". That's because they're actually strings, and while false is false, "false" is true. 
+# You can never, it turns out, test a boolean fact with a bare "if". That's because they're actually strings, and while false is false, "false" is true.
 # Interestingly, stdlib includes a function that can convert strings to booleans! So use that.
-if str2bool($is_virtual) {
-  notice("It's virtual!")
-}
-else {
-  notice("It's physical!")
-}
+# if str2bool($is_virtual) {
+#   notice("It's virtual!")
+# }
+# else {
+#   notice("It's physical!")
+# }
 
+if $is_virtual {
+  notice("Guess this is a virtual machine!")
+}
