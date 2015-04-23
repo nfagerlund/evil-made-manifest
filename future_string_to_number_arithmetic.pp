de037627c33bcd4e260f443077b197989515b21c
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-04-23 10:11:00 -0700

    Show use of double-negation operator to get around having to use scanf()

--- future_string_to_number_arithmetic.pp
+++ future_string_to_number_arithmetic.pp
@@ -12,3 +12,10 @@ notice(spew( "-077" ))
 # Notice: Scope(Class[main]): ["-077"]
 # [String]
 # ... if the operator is inside the quotes, it cancels out.
+
+# oh, something I just got curious about:
+$mystring = "899"
+notice(spew( --$mystring )) #
+# Notice: Scope(Class[main]): [899]
+# [Fixnum]
+# So there's a secret double-negation -- operator that converts strings to numbers.
