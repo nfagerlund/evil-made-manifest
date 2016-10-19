480aff827f02e4c3ccef6f96c704e652bcfda9ea
Nick Fagerlund <nick.fagerlund@gmail.com>
2016-10-18 19:13:55 -0700

    Update info about chaining arrays of resource references.

--- resource-reference-multi.pp
+++ resource-reference-multi.pp
@@ -1,15 +1,18 @@
-# What I learned: multi-references can be used anywhere an array of references can be used, but not vice-versa. 
+# What I learned: multi-references can be used anywhere an array of references can be used, but not vice-versa.
 # You can use a multi-reference with an attribute block, but not an array.
-# Additionally, collectors don't yield arrays of resource references; they're a special syntax. Can't use them as the value of an attribute, although you can order them with chain arrows. 
-# an array of resource refs can't be the object of a chain arrow! But a multi-ref can. 
+  # This remains true in 4.x, and actually kind of makes sense, since there's no guarantee that an array of references are all the same resource type. And it now gives a helpful error.
+# Additionally, collectors don't yield arrays of resource references; they're a special syntax. Can't use them as the value of an attribute, although you can order them with chain arrows.
+# an array of resource refs can't be the object of a chain arrow! But a multi-ref can.
+  # THIS HAS CHANGED, and in 4.x it's now legal to chain arrays.
 # Multi-refs can be included in arrays of references.
 # You can use an array (or variable containing an array) instead of a comma-separated list.
+  # I can't remember what I meant by this.
 notify {'third':
   tag => 'feh',
 }
 
 notify {'secondish':
-  before => Notify[ [ 'four', 'firstlyish', 'third'] ], 
+#   before => Notify[ [ 'four', 'firstlyish', 'third'] ],
   # before => Notify <| tag == feh |>,
 }
 notify {'firstlyish':
@@ -21,8 +24,13 @@ Notify['firstlyish', 'secondish', 'third'] {
   message => "HAHAHAHAHA",
 }
 
+# [ Notify['firstlyish'], Notify['secondish'], Notify['third'] ] {
+#   message => "HAHAHAHAHA",
+# }
+# Error: Could not parse for environment production: Expression is not valid as a resource, resource-default, or resource-override at /Users/nick/Documents/manifests/resource-reference-multi.pp:24:1 on node magpie.lan
+
 Notify <| tag == feh |> {
   message => 'overridden!',
 }
 
-# Notify['secondish'] -> [Notify['firstlyish'], Notify['third']]
+Notify['secondish'] -> [Notify['firstlyish'], Notify['third']]
